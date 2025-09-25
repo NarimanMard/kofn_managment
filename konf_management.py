a81a1212a029593a2bbea0676b48ee402817a7a7
Nariman <mardanovn654@gmail.com>
2025-09-25 20:26:30 +0300

    step1

--- konf_management.py
+++ konf_management.py
@@ -5,22 +5,24 @@ class CommandLineEmulator(tk.Tk):
         super().__init__()
 # 1 ркно
         self.title("VFS")
-        self.geometry("800x600")
-# Поле отображения истории ввода и вывода:
-        self.output = tk.Text(self, bg="#000000", fg="white", insertbackground="white")
+        self.wm_attributes("-fullscreen", True)  # Открываем окно на весь экран
+        self.output = tk.Text(self, bg="#000000", fg="white", insertbackground="white", font=("Arial", 18))
         self.output.pack(fill=tk.BOTH, expand=True)
         self.output.config(state="disabled")  # Сделаем поле вывода неактивным
     
 # Виджет для ввода команд
         input_frame = tk.Frame(self)
         input_frame.pack(side=tk.BOTTOM, fill=tk.X)
-        self.input_field = tk.Entry(input_frame)
-        self.input_field.pack(side=tk.LEFT, fill=tk.X, expand=True)
+        self.input_field = tk.Entry(input_frame, bg="#000000", fg="white",insertbackground="white", font=("Courier New", 20))
+        self.input_field.pack(side=tk.LEFT, fill=tk.X, expand=True, ipady=10)
         self.input_field.bind("<Return>", lambda event: self.execute_command())
-    
+
+# Ставим фокус на поле ввода при запуске
+        self.input_field.focus_set()
+
 # Кнопка отправки команды
-        submit_button = tk.Button(input_frame, text="Выполнить", font=("Arial", 12), height=2, width=10, bg="#5c5c5c", fg="white", 
-                                  activebackground="#7b7b7b", relief=tk.RAISED, borderwidth=2, command=self.execute_command)
+        submit_button = tk.Button(input_frame, text="Выполнить", font=("Arial", 15), height=2, width=15, bg="#5c5c5c", fg="white", 
+                                  activebackground="#7b7b7b", relief=tk.RAISED, borderwidth=25, command=self.execute_command)
         submit_button.pack(side=tk.RIGHT)
 
 #обработка ввод команд :
@@ -34,7 +36,7 @@ class CommandLineEmulator(tk.Tk):
             command = cmd_parts[0]
             args = cmd_parts[1:]
             
-            output = f"> {command} {' '.join(args)}\n"
+            output = f"~💲 {command} {' '.join(args)}\n"
             if command == 'ls':
                 output += f"{command}: {' '.join(args)}\n"
             elif command == 'cd':
@@ -42,13 +44,13 @@ class CommandLineEmulator(tk.Tk):
             elif command == 'exit':
                 self.destroy()
             elif command == "echo":
-                output += f"{' '.join(args)}\n"
+                output += f"{' '.join(args)}😬\n"
             else:
-                output += f"Ошибка: неизвестная команда '{command}'\n"
+                output += f"Ошибка: неизвестная команда '{command}'🤔\n"
                 
             self.display_output(output)
         except Exception as e:
-            self.display_output(f"Ошибка выполнения команды: {e}")
+            self.display_output(f"Ошибка выполнения команды: {e}😒")
     
         finally:
             self.input_field.delete(0, tk.END)
